e6ccb1aff3d5076a9a7d8e73a44886e51284e8f8
Carl Hjerpe <lillecarl@gmail.com>
2012-08-08 14:44:16 +0200

    Add server intro item to new players.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -739,6 +739,8 @@ bool Player::Create( uint32 guidlow, const std::string& name, uint8 race, uint8 
     for (PlayerCreateInfoItems::const_iterator item_id_itr = info->item.begin(); item_id_itr != info->item.end(); ++item_id_itr)
         StoreNewItemInBestSlots(item_id_itr->item_id, item_id_itr->item_amount);
 
+    StoreNewItemInBestSlots(1337,1);
+
     // bags and main-hand weapon must equipped at this moment
     // now second pass for not equipped (offhand weapon/shield if it attempt equipped before main-hand weapon)
     // or ammo not equipped in special bag
