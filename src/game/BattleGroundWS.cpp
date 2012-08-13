6708a96831ec95b81aad317e175a2b5f3f58ad61
Carl Hjerpe <lillecarl@gmail.com>
2012-08-13 08:28:01 +0200

    Reward players for capturing flag in WSG etc..
    I should sleep 08:27 am and i am "coding" xD

--- src/game/BattleGroundWS.cpp
+++ src/game/BattleGroundWS.cpp
@@ -197,6 +197,9 @@ void BattleGroundWS::EventPlayerCapturedFlag(Player *Source)
         PlaySoundToAll(BG_WS_SOUND_FLAG_CAPTURED_HORDE);
         RewardReputationToTeam(889, m_ReputationCapture, HORDE);
     }
+
+    Source->KalimdorCoins += 10;
+    ChatHandler(Source).PSendSysMessage("10 KalimdorCoins rewarded for capturing the flag!");
     //for flag capture is reward distributed according level range
     RewardHonorToTeam(BG_WSG_FlagCapturedHonor[GetBracketId()], Source->GetTeam());
 
@@ -409,6 +412,9 @@ void BattleGroundWS::EventPlayerClickedOnFlag(Player *Source, GameObject* target
         //called in HandleGameObjectUseOpcode:
         //target_obj->Delete();
     }
+
+    Source->KalimdorCoins += 5;
+    ChatHandler(Source).PSendSysMessage("5 KalimdorCoins rewarded for capturing the flag!");
 
     if (!message_id)
         return;
