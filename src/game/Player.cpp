04dd86e6f161e682e0d56cba75b13791988cebfd
Carl Hjerpe <lillecarl@gmail.com>
2012-08-26 22:17:46 +0200

    lets make players ffapvp in bgs, to go over some client limitation or smth.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1095,6 +1095,9 @@ void Player::Update( uint32 update_diff, uint32 p_time )
     if(!IsInWorld())
         return;
 
+    if (GetBattleGround())
+        SetFFAPvP(true);
+
     TenSTimer += update_diff;
     if (TenSTimer > 10000)
     {
